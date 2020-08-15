9c87951cecd867fa610d09cf88ca0da9ac27fb81
12YTC09 <p0361533@o365.fcu.edu.tw>
2020-08-15 14:03:15 +0800

    ch03

---
+++ ch03/StrKeyDict0.py
@@ -0,0 +1,23 @@
+#!/usr/bin/pydoc3.5 
+#-*- coding: UTF-8 -*-
+
+
+#3-7 將非字串的鍵轉換為字串
+class StrKeyDict0(dict):
+    
+    def __missing__(self,key):
+        if isinstance(key,str):
+            raise KeyError(key)
+        return self[str(key)]
+    def get(self,key,default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+    def __contains__(self,key):
+        return key in self.keys() or str(key) in self.keys()
+
+
+    
+
+
