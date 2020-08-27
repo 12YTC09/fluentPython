e7f87f160928b9fd4520b0355ec05045490a0a4c
12YTC09 <p0361533@o365.fcu.edu.tw>
2020-08-27 16:54:53 +0800

    ch03

--- ch03/hashdiff.py
+++ ch03/hashdiff.py
@@ -14,15 +14,20 @@ def hash_diff(o1,o2):
     print(h1)
     h2 = '{:>0{}b}'.format(hash(o2),MAX_BITS)
     print(h2)
-    #diff = ''.join('!' if b1 != b2 else ' ' for b1,b2 in zip(h1,h2))
-    #count = '!={}'.format(diff.count('!'))
-    
+    diff = ''.join('!' if b1 != b2 else ' ' for b1,b2 in zip(h1,h2))
+    count = '!={}'.format(diff.count('!'))
+    width = max(len(repr(o1)),len(repr(o2)),8)
+    sep = '-'* (width*2+MAX_BITS)
+    return '{!r:{width}} {}\n{:{width}} {} {}\n{!r:{width}} {}\n{}'.format(
+            o1,h1,''* width,diff,count,o2,h2,sep,width=width)
 
 
 
 if __name__=='__main__':
     print(hash_diff(1,1.0))
-
+    print(hash_diff(1.0,1.0001))
+    print(hash_diff(1.0001,1.0002))
+    print(hash_diff(1.0002,1.0003))
 
 
 
