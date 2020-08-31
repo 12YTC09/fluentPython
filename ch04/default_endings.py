44e513f649658125660cc4745958718f1ffd2081
12YTC09 <p0361533@o365.fcu.edu.tw>
2020-08-31 12:23:31 +0800

    ch04

---
+++ ch04/default_endings.py
@@ -0,0 +1,73 @@
+#4-11 探索編碼預設值
+'''
+locale.getpreferredencoding():
+return the encoding used for text data,according to user preferences.  
+
+---------------------------------------------------------------------------
+
+
+
+
+
+'''
+
+
+
+
+import sys,locale
+
+expressions = 
+"""
+            locale.getpreferredencoding()
+            type(my_file)
+            my_file.encoding
+            sys.stdout.isatty()
+            sys.stdout.encoding
+            sys.stdin.isatty()
+            sys.stdin.encoding
+            sys.stderr.isatty()
+            sys.stderr.encoding
+            sys.getdefaultencoding()
+            sys.getfilesystemencoding()
+            """
+
+
+
+
+
+
+my_file = open('dummy','w') 
+
+for expression in expressions.split():
+    value = eval(expression)
+    print(expression.rjust(30), '->' ,repr(value))
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
