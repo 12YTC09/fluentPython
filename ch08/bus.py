afbe132c2aae556f937509303909cbb03873c323
12YTC09 <p0361533@o365.fcu.edu.tw>
2020-09-13 17:32:12 +0800

    ch08

---
+++ ch08/bus.py
@@ -0,0 +1,13 @@
+#8-8 接受乘客的巴士
+
+class Bus:
+    def __init__(self,passengers = None):
+        if passengers is None:
+            self.passengers =[]
+        else:
+            self.passengers = list(passengers)
+    def pick(self,name):
+        self.passengers.append(name)
+    def drop(self,name):
+        self.passengers.remove(name)
+
